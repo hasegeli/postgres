33e4df9713e2a3149c94cb36841adf1f21e0fcb2
Emre Hasegeli <hasegeli@tart.com.tr>
2014-01-24 00:13:38 +0200

    Update the copyright year on network_selfuncs.c.

--- src/backend/utils/adt/network_selfuncs.c
+++ src/backend/utils/adt/network_selfuncs.c
@@ -6,7 +6,7 @@
  * Estimates are based on null fraction, distinct value count, most common
  * values, and histogram of inet, cidr datatypes.
  *
- * Portions Copyright (c) 1996-2013, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2014, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
